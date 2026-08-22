4b60cb80e2e9e121ccc6119a6f7754b844039049
Augusto Tavares Dias <augustotdias@gmail.com>
2026-08-22 15:12:39 +0100

    Add root layout

---
+++ app/layout.jsx
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'ATD WORKS',description:'Artwork archive by Augusto Tavares Dias'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
